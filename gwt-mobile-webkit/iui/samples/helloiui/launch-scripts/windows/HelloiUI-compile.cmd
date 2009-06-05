fe39b293b5c3fe62ab8e481244824822305bbe4c
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2009-06-05 11:40:16 +0000

    Initial import of iUI API

---
+++ gwt-mobile-webkit/iui/samples/helloiui/launch-scripts/windows/HelloiUI-compile.cmd
@@ -0,0 +1,7 @@
+@if "%GWT_HOME%"=="" goto needGWTHome
+@java -Xmx128M -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-mobile-webkit-iui.jar" com.google.gwt.dev.Compiler -war "%~dp0\..\..\www" %* com.google.code.gwt.iui.sample.iui.HelloiUI
+@exit /B %ERRORLEVEL%
+
+:needGWTHome
+@echo The environment variable GWT_HOME is not defined, it should point to a valid GWT installation.
+@exit /B 1
