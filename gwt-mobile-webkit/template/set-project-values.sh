8aa5da28ed2444652cdb9b868806f0d3906268f7
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2009-06-05 11:48:08 +0000

    Initial import of tools and project template

---
+++ gwt-mobile-webkit/template/set-project-values.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Please set your values in this script before executing!!
+
+TEMPLATE_TITLE="Database"
+TEMPLATE_ID="database"
+TEMPLATE_CAPS="DATABASE"
+
+TEMPFILE="/tmp/sed$PID.tmp"
+
+function fdren {
+  echo "Renaming files/dirs *$1* to *$2*..."
+  for FILENAME in `find . -name *$1*`; do
+    echo "$FILENAME" | sed -e "s/$1/$2/g" > $TEMPFILE
+    NEWNAME=`cat $TEMPFILE`
+    if [ -d $FILENAME ]; then
+      echo "Renaming directory $FILENAME to $NEWNAME..."
+      mkdir -p $NEWNAME
+      mv $FILENAME/* $NEWNAME
+      rm -Rf $FILENAME
+    else
+      echo "Renaming file $FILENAME to $NEWNAME..."
+      mv $FILENAME $NEWNAME
+    fi
+  done
+}
+
+function runrenames {
+  fdren template $TEMPLATE_ID
+  fdren Template $TEMPLATE_TITLE
+  fdren TEMPLATE $TEMPLATE_CAPS
+}
+
+# run renames multiple times because of nested folder renames:
+runrenames
+runrenames
+
+# rename in file content:
+for FILENAME in `find . -name '*.xml' -type f` \
+                `find . -name '*.properties' -type f` \
+                `find . -name '*.conf' -type f` \
+                `find . -name '*.txt' -type f` \
+                `find . -name '*.css' -type f` \
+                `find . -name '*.html' -type f` \
+                `find . -name '*.java' -type f` \
+                `find . -name '*.' -type f` \
+                `find . -name '*.cmd' -type f` \
+                `find . -name '*.bat' -type f` \
+                `find . -name '*.sh' -type f`; do
+
+  if [ $FILENAME != "./set-project-values.sh" ]; then
+    sed -e "s/Template/$TEMPLATE_TITLE/g" \
+        -e "s/template/$TEMPLATE_ID/g" \
+        -e "s/TEMPLATE/$TEMPLATE_CAPS/g" \
+        $FILENAME > $TEMPFILE
+
+    if [ -s $TEMPFILE ] && [ "`cat $TEMPFILE`" != "`cat $FILENAME`" ]; then
+      echo "Finding and Replacing in file: $FILENAME..."
+      mv $TEMPFILE $FILENAME
+    fi
+  fi
+done
+
+echo "Remove .svn directories? enter your root pwd!"
+sudo find . -name .svn -exec rm -Rf {} ";"
+
+# removing this script:
+rm -f set-project-values.sh
