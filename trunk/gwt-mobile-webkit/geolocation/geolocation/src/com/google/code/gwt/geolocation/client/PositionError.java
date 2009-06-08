8d169396626f089c77b18766c9662ad2316b4b4e
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2009-06-08 22:19:09 +0000

    Added HTML5 Geolocation API with sample

---
+++ trunk/gwt-mobile-webkit/geolocation/geolocation/src/com/google/code/gwt/geolocation/client/PositionError.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2009 Bart Guijt and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.code.gwt.geolocation.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+/**
+ * Represents an error encountered when asking the (current) {@link Position}.
+ * 
+ * @author bguijt
+ * @see <a href="http://www.w3.org/TR/geolocation-API/#position-error">W3C Geolocation API - PositionError interface</a>
+ */
+public class PositionError extends JavaScriptObject {
+
+  public static final int UNKNOWN_ERROR = 0;
+  public static final int PERMISSION_DENIED = 1;
+  public static final int POSITION_UNAVAILABLE = 2;
+  public static final int TIMEOUT= 3;
+  
+  protected PositionError() {}
+  
+  /**
+   * Returns the error code. Must be one of {@link #UNKNOWN_ERROR}, {@link #PERMISSION_DENIED}, {@link #POSITION_UNAVAILABLE} or {@link #TIMEOUT}.
+   * 
+   * @return the error code
+   * @see <a href="http://www.w3.org/TR/geolocation-API/#code">W3C Geolocation API - PositionError.code</a>
+   */
+  public final native int getCode() /*-{
+    return this.code;
+  }-*/;
+  
+  /**
+   * Returns the message describing the details of the error encountered.
+   * 
+   * @return the error message
+   * @see <a href="http://www.w3.org/TR/geolocation-API/#message">W3C Geolocation API - PositionError.message</a>
+   */
+  public final native String getMessage() /*-{
+    return this.message;
+  }-*/;
+}
---
+++ trunk/gwt-mobile-webkit/geolocation/geolocation/src/com/google/code/gwt/geolocation/client/PositionOptions.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2009 Bart Guijt and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.code.gwt.geolocation.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+/**
+ * Represents options to configure the {@link Geolocation} operations.
+ * 
+ * @author bguijt
+ * @see <a href="http://www.w3.org/TR/geolocation-API/#position-options">W3C
+ *      Geolocation API - PositionOptions interface</a>
+ */
+public class PositionOptions extends JavaScriptObject {
+
+  protected PositionOptions() {
+  }
+
+  /**
+   * Creates a new PositionOptions object to use in the {@link Geolocation}
+   * functions.
+   * 
+   * @param enableHighAccuracy a hint that the application would like to receive
+   *          the best possible results
+   * @param timeout denotes the maximum length of time (expressed in
+   *          milliseconds) that is allowed to pass from the the call to
+   *          getCurrentPosition() or watchPosition() until the corresponding
+   *          PositionCallback is invoked
+   * @param maximumAge indicates that the application is willing to accept a
+   *          cached position whose age is no greater than the specified time in
+   *          milliseconds
+   * @return a new PositionOptions instance
+   */
+  public static final native PositionOptions getPositionOptions(
+      boolean enableHighAccuracy, int timeout, int maximumAge) /*-{
+    return {enableHighAccuracy: enableHighAccuracy,
+      timeout: timeout,
+      maximumAge: maximumAge};
+  }-*/;
+
+  public final native boolean isEnableHighAccuracy() /*-{
+    return this.enableHighAccuracy;
+  }-*/;
+
+  public final native int getTimeout() /*-{
+    return this.timeout;
+  }-*/;
+
+  public final native int getMaximumAge() /*-{
+    return this.maximumAge;
+  }-*/;
+}
