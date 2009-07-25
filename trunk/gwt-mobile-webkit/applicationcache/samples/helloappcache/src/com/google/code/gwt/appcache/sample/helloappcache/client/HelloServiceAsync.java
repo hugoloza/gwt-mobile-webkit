b01f7ec26028ff21686790892e2a82c279405bcf
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2009-07-25 21:38:25 +0000

    javadoc

--- trunk/gwt-mobile-webkit/applicationcache/samples/helloappcache/src/com/google/code/gwt/appcache/sample/helloappcache/client/HelloServiceAsync.java
+++ trunk/gwt-mobile-webkit/applicationcache/samples/helloappcache/src/com/google/code/gwt/appcache/sample/helloappcache/client/HelloServiceAsync.java
@@ -2,6 +2,13 @@ package com.google.code.gwt.appcache.sample.helloappcache.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+/**
+ * Defines just some demo service. The point is that the specified
+ * RemoteServiceRelativePath will result in a NETWORK entry in the cache
+ * manifest.
+ * 
+ * @author bguijt
+ */
 public interface HelloServiceAsync {
 
   void sayHello(String name, AsyncCallback<String> callback);
