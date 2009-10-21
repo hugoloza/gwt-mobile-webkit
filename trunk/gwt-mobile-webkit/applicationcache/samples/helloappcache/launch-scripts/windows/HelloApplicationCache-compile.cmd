350abb0bc8890747f0b00ff0b5d19c7154858398
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2009-10-21 20:44:39 +0000

    Revised the build of the appcache subproject to deal with the following issues: - keep compatibility with GWT's build system (the 'build-tools' part); - use GWT1.6-style compiler/hostedmode (war layout); - encourage easier build.xml/common.ant.xml synchronization across subprojects.

--- trunk/gwt-mobile-webkit/applicationcache/samples/helloappcache/launch-scripts/windows/HelloApplicationCache-compile.cmd
+++ trunk/gwt-mobile-webkit/applicationcache/samples/helloappcache/launch-scripts/windows/HelloApplicationCache-compile.cmd
@@ -1,5 +1,5 @@
 @if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx128M -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-mobile-webkit-appcache.jar" com.google.gwt.dev.Compiler -war "%~dp0\..\..\www" %* com.google.code.gwt.appcache.sample.helloappcache.HelloApplicationCache
+@java -Xmx128M -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-html5-appcache.jar" com.google.gwt.dev.Compiler -war "%~dp0\..\..\www" %* com.google.code.gwt.appcache.sample.helloappcache.HelloApplicationCache
 @exit /B %ERRORLEVEL%
 
 :needGWTHome
