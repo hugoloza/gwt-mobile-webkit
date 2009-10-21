90163891260a45a2c1db230a20bfd10085579c84
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2009-10-21 22:59:47 +0000

    Revised the build of the storage subproject as a copy from the appcache subproject.
    Also adjusted the launch scripts, added a war directory to use GWT1.6-style HostedMode.

--- trunk/gwt-mobile-webkit/storage/samples/hellostorage/launch-scripts/windows/HelloStorage-shell.cmd
+++ trunk/gwt-mobile-webkit/storage/samples/hellostorage/launch-scripts/windows/HelloStorage-shell.cmd
@@ -1,5 +1,5 @@
 @if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx128m -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-mobile-webkit-storage.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.code.gwt.storage.sample.storage.HelloStorage/HelloStorage.html
+@java -Xmx128m -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-html5-storage.jar" com.google.gwt.dev.HostedMode -war "%~dp0\..\..\war" %* -startupUrl HelloStorage.html com.google.code.gwt.storage.sample.hellostorage.HelloStorage
 @exit /B %ERRORLEVEL%
 
 :needGWTHome
