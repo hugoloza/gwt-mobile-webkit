3a0a4769fc5eed32989f03d51d8265ae3c7376b6
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2009-10-21 22:40:31 +0000

    Revised the build of the geolocation subproject as a copy from the appcache subproject.
    Also adjusted the launch scripts, added a war directory to use GWT1.6-style HostedMode.

--- trunk/gwt-mobile-webkit/geolocation/samples/hellogeolocation/launch-scripts/windows/HelloGeolocation-compile.cmd
+++ trunk/gwt-mobile-webkit/geolocation/samples/hellogeolocation/launch-scripts/windows/HelloGeolocation-compile.cmd
@@ -1,5 +1,5 @@
 @if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx128M -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-mobile-webkit-geolocation.jar" com.google.gwt.dev.Compiler -war "%~dp0\..\..\www" %* com.google.code.gwt.geolocation.sample.geolocation.HelloGeolocation
+@java -Xmx128M -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-html5-geolocation.jar" com.google.gwt.dev.Compiler -war "%~dp0\..\..\war" %* com.google.code.gwt.geolocation.sample.geolocation.HelloGeolocation
 @exit /B %ERRORLEVEL%
 
 :needGWTHome
