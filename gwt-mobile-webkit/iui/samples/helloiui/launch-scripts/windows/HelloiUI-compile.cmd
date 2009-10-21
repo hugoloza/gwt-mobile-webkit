987b9fdb5e39662c49d24e24ff60609077b05845
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2009-10-21 23:43:35 +0000

    Revised the build of the iui subproject as a copy from the appcache subproject.
    Also adjusted the launch scripts, added a war directory to use GWT1.6-style HostedMode.

--- gwt-mobile-webkit/iui/samples/helloiui/launch-scripts/windows/HelloiUI-compile.cmd
+++ gwt-mobile-webkit/iui/samples/helloiui/launch-scripts/windows/HelloiUI-compile.cmd
@@ -1,5 +1,5 @@
 @if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx128M -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-mobile-webkit-iui.jar" com.google.gwt.dev.Compiler -war "%~dp0\..\..\www" %* com.google.code.gwt.iui.sample.iui.HelloiUI
+@java -Xmx128M -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-mobile-webkit-iui.jar" com.google.gwt.dev.Compiler -war "%~dp0\..\..\war" %* com.google.code.gwt.iui.sample.iui.HelloiUI
 @exit /B %ERRORLEVEL%
 
 :needGWTHome
