615fabce2eac5cd26edb8087edca68734f662bdf
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2009-10-21 22:20:40 +0000

    Revised the build of the database subproject as a copy from the appcache subproject.
    Also adjusted the launch scripts to use GWT1.6-style HostedMode.

--- trunk/gwt-mobile-webkit/database/samples/hellodatabase/launch-scripts/windows/HelloDatabase-shell.cmd
+++ trunk/gwt-mobile-webkit/database/samples/hellodatabase/launch-scripts/windows/HelloDatabase-shell.cmd
@@ -1,5 +1,5 @@
 @if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx128m -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-mobile-webkit-database.jar" com.google.gwt.dev.GWTShell -out "%~dp0\..\..\www" %* com.google.code.gwt.database.sample.database.HelloDatabase/HelloDatabase.html
+@java -Xmx128m -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-html5-database.jar" com.google.gwt.dev.HostedMode -war "%~dp0\..\..\war" %* -startupUrl HelloDatabase.html com.google.code.gwt.database.sample.database.HelloDatabase
 @exit /B %ERRORLEVEL%
 
 :needGWTHome
