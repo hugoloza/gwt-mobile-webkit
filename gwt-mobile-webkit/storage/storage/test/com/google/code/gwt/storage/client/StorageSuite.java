522f39a06c7f2078ed6dc90b888d8408d93aa7d8
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2009-11-10 12:55:41 +0000

    Fixed the Storage testcase

---
+++ gwt-mobile-webkit/storage/storage/test/com/google/code/gwt/storage/client/StorageSuite.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2009 Bart Guijt and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.code.gwt.storage.client;
+
+import junit.framework.Test;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+
+/**
+ * Suite for all Storage tests.
+ * 
+ * @author bguijt
+ */
+public class StorageSuite {
+
+  public static Test suite() {
+    GWTTestSuite suite = new GWTTestSuite("Test for HTML5 Storage API");
+    
+    // $JUnit-BEGIN$
+   suite.addTestSuite(StorageTest.class);
+    // $JUnit-END$
+    
+    return suite;
+  }
+}
