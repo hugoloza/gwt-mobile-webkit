1694b1ac232a3e6396538adbd516ed5236e80473
bguijt <bguijt@d4a110ee-3d6d-11de-bd56-27b9897bf3d5>
2010-01-21 21:33:10 +0000

    Implements Issue #12.
    The shell scripts to launch the sample apps from Linux/Mac/Windows commandline are now all sources from the same templates.
    
    Added a -run script to help run the samples in compiled/production mode.

--- gwt-mobile-webkit/database/samples/hellodatabase/launch-scripts/windows/HelloDatabase-compile.cmd
+++
@@ -1,7 +0,0 @@
-@if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx128M -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-html5-database.jar" com.google.gwt.dev.Compiler -war "%~dp0\..\..\war" %* com.google.code.gwt.database.sample.database.HelloDatabase
-@exit /B %ERRORLEVEL%
-
-:needGWTHome
-@echo The environment variable GWT_HOME is not defined, it should point to a valid GWT installation.
-@exit /B 1
--- gwt-mobile-webkit/geolocation/samples/hellogeolocation/launch-scripts/windows/HelloGeolocation-shell.cmd
+++
@@ -1,7 +0,0 @@
-@if "%GWT_HOME%"=="" goto needGWTHome
-@java -Xmx128m -cp "%~dp0\..\..\src;%~dp0\..\..\bin;%GWT_HOME%\gwt-user.jar;%GWT_HOME%\gwt-dev-windows.jar;..\..\..\..\gwt-html5-geolocation.jar" com.google.gwt.dev.HostedMode -war "%~dp0\..\..\war" %* -startupUrl HelloGeolocation.html com.google.code.gwt.geolocation.sample.geolocation.HelloGeolocation
-@exit /B %ERRORLEVEL%
-
-:needGWTHome
-@echo The environment variable GWT_HOME is not defined, it should point to a valid GWT installation.
-@exit /B 1
